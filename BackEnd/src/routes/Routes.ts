356d063d5aba83cd8383440586a47900c693e4b5
matheus-nbx52 <matheusfssilva2001@gmail.com>
2023-01-23 20:01:49 -0300

    adição das rotas update e delete

--- BackEnd/src/routes/Routes.ts
+++ BackEnd/src/routes/Routes.ts
@@ -1,12 +1,26 @@
 import controlerS3 from "../controller/ControleS3";
 import upload from "../middleware/upload";
-
-import { Router } from "express";
+import UploadArchives from "../middleware/UploadArchives";
+import DeleteArchives from "../middleware/DeleteArchives";
+import { response, Router } from "express";
+import multer from "multer";
 ("express");
 
 const routes = Router();
 
-routes.post("/", upload.single("userItem"), controlerS3.create);
+routes.post("/", upload.single('image'), async ( request, response ) => {
+    const uploadArchives = new UploadArchives();
+    await uploadArchives.execute();
+
+    return response.send();
+});
+
+routes.delete("/", async (request, response) => {
+    const deleteArchives = new DeleteArchives();
+    await deleteArchives.execute();
+
+    return response.send();
+})
 
 
 export default routes;
