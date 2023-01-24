25aafec12aa9ca2362e79704bf12ea3b24d1d858
edsay12 <edvandearaujo2@hotmail.com>
2023-01-24 18:52:12 -0300

    bug na fonte resolvido

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -7,7 +7,7 @@ import { Armazenamento } from "./pages/armazenamento/Armazenamento";
 import { PageContainer } from "./components/pageContainer/PageContainer";
 import { PageTitle } from "./components/pageTitle/pageTitle";
 import { Home } from "./pages/home/home";
-import Login from "./pages/login/login";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,7 +21,7 @@ function App() {
       <Route path="/sair" element={<DeskBoard children={<h1>sair</h1>}/>} />
       <Route path="/configuraçoes" element={<DeskBoard children={<h1>configuraçoes</h1>}/>} />
       <Route path="/help" element={<DeskBoard children={<h1>help</h1>}/>} />
-      <Route path="/login" element={<Login/>} />
+      
     </Routes>
   );
 }
