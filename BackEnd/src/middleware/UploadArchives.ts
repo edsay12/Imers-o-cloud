3b6400f04b1f079518ace1ec7d8ff764e26c93fb
matheus-nbx52 <matheusfssilva2001@gmail.com>
2023-01-24 18:50:50 -0300

    atualização da classe

--- BackEnd/src/middleware/UploadArchives.ts
+++ BackEnd/src/middleware/UploadArchives.ts
@@ -1,6 +1,11 @@
+import ControleS3 from "../controller/ControleS3"
+
 class UploadArchives {
-    async execute(): Promise<void> {
+    async execute(file: Express.Multer.File): Promise<void> {
       //executa o serviço de upload
+      const controlerS3 = new ControleS3();
+
+      await controlerS3.saveFile(file.filename);
     }
 }
 
