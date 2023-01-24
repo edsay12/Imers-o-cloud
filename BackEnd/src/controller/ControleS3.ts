319de825a0f3d498b79736b8fc6e8ba47eeecb8d
matheus-nbx52 <matheusfssilva2001@gmail.com>
2023-01-24 18:50:28 -0300

    adição de configurações do controleS3

--- BackEnd/src/controller/ControleS3.ts
+++ BackEnd/src/controller/ControleS3.ts
@@ -1,14 +1,49 @@
 import { Request, Response } from "express";
-
+import aws, { S3 } from 'aws-sdk';
+import mime from "mime";
+import upload from "../middleware/upload";
+import path from "path";
+import FileSystem from "fs";
 
 class ControlerS3 {
-  // Create a item in s3
-  create(req: Request, res: Response) {
-    console.log(req.file?.filename);
-    res.sendStatus(200);
-  }
+  private client: S3;
 
-  update() {}
+  constructor() {
+    this.client = new aws.S3({
+      region: 'us-east-1',
+    });
+  }
+  async saveFile(filename: string): Promise<void>{
+    const originalPath = path.resolve(__dirname, "../../public/upload");
+
+    const ContentType = mime.getType(originalPath);
+
+    if (!ContentType) {
+      //console.log("Arquivo não encontrado")
+      throw new Error("file not found");
+    }
+
+    const fileContent = await FileSystem.promises.readFile(originalPath)
+
+    this.client.putObject({
+      Bucket: 'files-users',
+      Key: filename,
+      ACL: 'public-read',
+      Body: fileContent,
+      ContentType,
+    })
+      .promise();
+    await FileSystem.promises.unlink(originalPath)
+    
+  }
+  
+  async deleteFile(filename: string): Promise<void>{
+    await this.client.deleteObject({
+      Bucket: 'filers users',
+      Key: filename,
+    })
+      .promise();
+  }
 }
 
-export default new ControlerS3();
+export default ControlerS3;
