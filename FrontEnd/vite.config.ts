2bdc5928d6138df0ec214bc79f78fcc123014249
edsay12 <edvandearaujo2@hotmail.com>
2023-04-06 08:33:38 -0300

    docker initial config

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: { alias: { "@": "/src" } },
   plugins: [react()],
-})
+  server: {
+    host: true, // needed for the DC port mapping to work
+    strictPort: true,
+    port: 3000,
+  },
+});
